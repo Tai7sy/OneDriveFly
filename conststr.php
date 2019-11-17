3155bbb0b4e9f8c420b454e4ce062242cacbfdfb
qkqpttgf <45693631+qkqpttgf@users.noreply.github.com>
2019-11-17 13:05:33 +0800

    设置多域名时的BUG，改格式

--- conststr.php
+++ conststr.php
@@ -16,7 +16,7 @@ adminloginpage ：管理登录的页面不再是'?admin'，而是此设置的值
 public_path    ：使用API长链接访问时，显示网盘文件的路径，不设置时默认为根目录；  
            　　　不能是private_path的上级（public看到的不能比private多，要么看到的就不一样）。  
 private_path   ：使用自定义域名访问时，显示网盘文件的路径，不设置时默认为根目录。  
-domain_path    ：格式为a1.com=/dir/path1;b1.com=/path2，比private_path优先。  
+domain_path    ：格式为a1.com:/dir/path1|b1.com:/path2，比private_path优先。  
 imgup_path     ：设置图床路径，不设置这个值时该目录内容会正常列文件出来，设置后只有上传界面，不显示其中文件（登录后显示）。  
 passfile       ：自定义密码文件的名字，可以是'pppppp'，也可以是'aaaa.txt'等等；  
         　       密码是这个文件的内容，可以空格、可以中文；列目录时不会显示，只有知道密码才能查看或下载此文件。  
@@ -76,8 +76,8 @@ $constStr = [
             'zh-cn' => '如果设置，登录按钮及页面隐藏。管理登录的页面不再是\'?admin\'，而是\'?此设置的值\'。',
         ],
         'domain_path' => [
-            'en-us' => 'more custom domain, format is a1.com=/dir/path1;b2.com=/path2',
-            'zh-cn' => '使用多个自定义域名时，指定每个域名看到的目录。格式为a1.com=/dir/path1;b1.com=/path2，比private_path优先。',
+            'en-us' => 'more custom domain, format is a1.com:/dir/path1|b2.com:/path2',
+            'zh-cn' => '使用多个自定义域名时，指定每个域名看到的目录。格式为a1.com:/dir/path1|b1.com=/path2，比private_path优先。',
         ],
         'imgup_path' => [
             'en-us' => 'Set guest upload dir, before set this, the files in this dir will show as normal.',
