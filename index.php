551572e283c090b302f42a80498cc35d64683667
qkqpttgf <45693631+qkqpttgf@users.noreply.github.com>
2019-12-04 16:11:06 +0800

    上次的不存在文件一直循环的bug改3次

--- index.php
+++ index.php
@@ -57,7 +57,7 @@ function main_handler($event, $context)
         $url = path_format($_SERVER['PHP_SELF'] . '/');
         return output('<script>alert(\''.$constStr['SetSecretsFirst'][$constStr['language']].'\');</script>', 302, [ 'Location' => $url ]);
     }
-
+    $_SERVER['retry'] = 0;
     return list_files($path);
 }
 
@@ -269,9 +269,12 @@ function list_files($path)
     }
     if ( isset($files['folder']) || isset($files['file']) ) {
         return render_list($path, $files);
-    } elseif (!isset($files['Error'])) {
+    } elseif (isset($files['error'])) {
+	    return output('<div style="margin:8px;">' . $files['error']['message'] . '</div>', 404);
+    } else {
         echo 'Error $files' . json_encode($files, JSON_PRETTY_PRINT);
-        return list_files($path);
+        $_SERVER['retry']++;
+        if ($_SERVER['retry']>3) return list_files($path);
     }
 }
 
@@ -624,7 +627,7 @@ function render_list($path, $files)
         body{font-family:'Helvetica Neue',Helvetica,Arial,sans-serif;font-size:14px;line-height:1em;background-color:#f7f7f9;color:#000}
         a{color:#24292e;cursor:pointer;text-decoration:none}
         a:hover{color:#24292e}
-        .title{text-align:center;margin-top:2rem;letter-spacing:2px;margin-bottom:2rem}
+        .title{text-align:center;margin-top:1rem;letter-spacing:2px;margin-bottom:2rem}
         .title a{color:#333;text-decoration:none}
         .list-wrapper{width:80%;margin:0 auto 40px;position:relative;box-shadow:0 0 32px 0 rgb(128,128,128);border-radius:15px;}
         .list-container{position:relative;overflow:hidden;border-radius:15px;}
