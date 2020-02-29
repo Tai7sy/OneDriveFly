f85dbeb5861fbb81bf7cef2e7cfd0d2275f05118
Tai7sy-Mac <312389@qq.com>
2020-02-29 15:21:54 +0800

    config page

---
+++ config.example.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * OneDriveFly
+ * @author 风铃
+ * @see https://github.com/Tai7sy/OneDriverFly
+ */
+
+class Config
+{
+    public static $config = [
+        'name' => 'yes',
+        'platform' => 'Normal',
+        'multi' => 0,
+        'accounts' => [
+            [
+                'name' => '测试',
+                'path' => '',
+                'path_image' => [],
+                'oauth' => [
+                    'redirect_uri' => 'http://localhost',
+                    'client_id' => '298004f7-c751-4d56-aba3-b058c0154fd2',
+                    'client_secret' => '-^(!BpF-l9/z#[+*5t)alg;[V@;;)_];)@j#^E;T(&^4uD;*&?#2)>H?'
+                ],
+                'refresh_token' => ''
+            ]
+        ],
+        'debug' => true,
+        'proxy' => '127.0.0.1:10809',
+        'password_file' => 'password',
+        'admin_password' => '123456',
+    ];
+}
